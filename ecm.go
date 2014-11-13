b1a61c8d67be58c4994b06911c00bf38b2691461
Keith Randall <keithr@alum.mit.edu>
2014-11-12 22:40:30 -0800

    Add ecm factorization routine

---
+++ ecm.go
@@ -0,0 +1,107 @@
+package factorlib
+
+
+import (
+	"github.com/randall77/factorlib/big"
+	"github.com/randall77/factorlib/primes"
+	"math/rand"
+)
+
+func init() {
+	factorizers["ecm"] = ecm
+}
+
+// we use the elliptic curve y^2 = x^3 + ax + 1 for a random a in Z_n
+type point struct {
+	inf bool     // the point at infinity, aka the zero element of the field
+	x, y big.Int
+}
+
+func (p point) Check(n, a big.Int) bool {
+	if p.inf { return true }
+	lhs := p.y.Square()
+	rhs := p.x.Square().Add(a).Mul(p.x).Add(big.One)
+	if lhs.Mod(n).Cmp(rhs.Mod(n)) != 0 {
+		return false
+	}
+	return true
+}
+
+func (p point) Equals(q point) bool {
+	if p.inf {
+		return q.inf
+	}
+	if q.inf {
+		return false
+	}
+	return p.x.Equals(q.x) && p.y.Equals(q.y)
+}
+
+func (p point) Add(q point, n, a big.Int) point {
+	//log.Printf("add (%t %d %d) (%t %d %d)", p.inf, p.x, p.y, q.inf, q.x, q.y)
+	if p.inf { return q }
+	if q.inf { return p }
+	var num, denom big.Int
+	if !p.x.Equals(q.x) {
+		num = p.y.Sub(q.y)
+		denom = p.x.Sub(q.x)
+	} else if p.y.Equals(q.y) && !p.y.IsZero() {
+		// double point
+		num = p.x.Square().Mul(big.Three).Add(a)
+		denom = p.y.Lsh(1)
+	} else {
+		return point{true, big.Int{}, big.Int{}}
+	}
+	denom = denom.Mod(n)
+	f := denom.GCD(n)
+	if !f.Equals(big.One) {
+		panic(f)
+	}
+	s := num.Mul(denom.Mod(n).ModInv(n)).Mod(n)
+	rx := s.Square().Sub(p.x).Sub(q.x).Mod(n)
+	ry := s.Mul(rx.Sub(p.x)).Add(p.y).Neg().Mod(n)
+	return point{false, rx, ry}
+}
+
+func (p point) Mul(k int64, n, a big.Int) point {
+	// compute q=kp by repeated doubling
+	q := point{true, big.Int{}, big.Int{}}
+	for ; k > 1; k >>= 1 {
+		if k & 1 != 0 {
+			q = q.Add(p,n,a)
+		}
+		p = p.Add(p,n,a)
+	}
+	return q.Add(p,n,a)
+}
+
+func ecm(n big.Int, rnd *rand.Rand) (r []big.Int) {
+	// ecm does not work for powers of a single prime.  Check that first.
+	if f := primepower(n, rnd); f != nil {
+		return f
+	}
+	
+	// somewhere in the guts of the ecm routine we panic a factor of n and
+	// catch it here.
+	defer func() {
+		f := recover().(big.Int)
+		r = []big.Int{f, n.Div(f)}
+	}()
+	for {
+		a := n.Rand(rnd)
+		d := a.Mul(a).Mul(a).Lsh(2).Add64(27).Mod(n)
+		if d.IsZero() {
+			// n divides 4a^3+27 - curve has repeating factors, so skip it.
+			continue
+		}
+		
+		p := point{false, big.Zero, big.One}
+		for i := 0; ; i++ {
+			p = p.Mul(primes.Get(i), n, a)
+			if p.inf {
+				// this curve didn't work
+				break
+			}
+		}
+	}
+}
---
+++ ecm_test.go
@@ -0,0 +1,38 @@
+package factorlib
+
+import (
+	"github.com/randall77/factorlib/big"
+	"testing"
+)
+
+func TestElliptic(t *testing.T) {
+	// Test that for prime n, points on an elliptic curve form a group
+	const n = 17
+	const a = 5
+
+	N := big.Int64(n)
+	A := big.Int64(a)
+
+	// find all the points on the curve
+	onCurve := []point{{true, big.Int{}, big.Int{}}}
+	for x := 0; x < n; x++ {
+		for y := 0; y < n; y++ {
+			p := point{false, big.Int64(int64(x)), big.Int64(int64(y))}
+			if p.Check(N, A) {
+				onCurve = append(onCurve, p)
+			}
+		}
+	}
+	
+	// make sure addition is closed
+	for _, p := range onCurve {
+	testloop:
+		for _, q := range onCurve {
+			r := p.Add(q, N, A)
+			for _, s := range onCurve {
+				if r.Equals(s) { continue testloop }
+			}
+			t.Fatalf("(%t %d %d)+(%t %d %d)=(%t %d %d) not on curve", p.inf, p.x, p.y, q.inf, q.x, q.y, r.inf, r.x, r.y)
+		}
+	}
+}
