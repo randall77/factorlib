c5cdaa8ab7d4f5410d6e49f956864b9d59dab068
Keith Randall <keithr@alum.mit.edu>
2019-05-30 14:20:09 -0400

    Fix up tests.

--- math/math_test.go
+++ math/math_test.go
@@ -1,4 +1,4 @@
-package factorlib
+package math
 
 import (
 	"math/rand"
@@ -22,8 +22,8 @@ func TestGCD(t *testing.T) {
 					g = z
 				}
 			}
-			if gcd(x, y) != g {
-				t.Errorf("gcd(%d,%d)=%d, want %d", x, y, gcd(x, y), g)
+			if GCD(x, y) != g {
+				t.Errorf("GCD(%d,%d)=%d, want %d", x, y, GCD(x, y), g)
 			}
 		}
 	}
@@ -32,10 +32,10 @@ func TestGCD(t *testing.T) {
 func TestModInv(t *testing.T) {
 	for n := int64(2); n < 1000; n++ {
 		for x := int64(1); x < n; x++ {
-			if gcd(n, x) != 1 {
+			if GCD(n, x) != 1 {
 				continue
 			}
-			y := modInv(x, n)
+			y := ModInv(x, n)
 			if x*y%n != 1 {
 				t.Errorf("n=%d x=%d y=%d xy=%d", n, x, y, x*y%n)
 			}
@@ -55,7 +55,7 @@ func TestQR(t *testing.T) {
 
 		for a := int64(0); a < p; a++ {
 			_, isQR := m[a]
-			r := quadraticResidue(a, p)
+			r := QuadraticResidue(a, p)
 			if r != isQR {
 				t.Errorf("p=%d a=%d want %t, got %t", p, a, isQR, r)
 			}
@@ -80,7 +80,7 @@ func TestSqrtModP(t *testing.T) {
 				// a is a quadratic nonresidue
 				continue
 			}
-			r := sqrtModP(a, p, rnd)
+			r := SqrtModP(a, p, rnd)
 			if r != s && r != p-s {
 				t.Errorf("p=%d a=%d want %d or %d, got %d", p, a, s, p-s, r)
 			}
@@ -93,7 +93,7 @@ func TestSqrtModPK(t *testing.T) {
 	for i := 0; i < 1000; i++ {
 		p := primes.Get(i)
 		for k := uint(1); ; k++ {
-			pk := exp(p, k)
+			pk := Exp(p, k)
 			if pk > 10000 {
 				break
 			}
@@ -105,7 +105,7 @@ func TestSqrtModPK(t *testing.T) {
 			}
 
 			for a := int64(0); a < pk; a++ {
-				if a != 0 && gcd(a, pk) != 1 {
+				if a != 0 && GCD(a, pk) != 1 {
 					// a is not relatively prime to p^k
 					continue
 				}
@@ -114,7 +114,7 @@ func TestSqrtModPK(t *testing.T) {
 					// a is a quadratic nonresidue
 					continue
 				}
-				r := sqrtModPK(a, p, k, rnd)
+				r := SqrtModPK(a, p, k, rnd)
 				ok = false
 				for _, x := range s {
 					if x == r {
@@ -133,34 +133,34 @@ func TestSqrtModPK(t *testing.T) {
 func TestSqrtModN(t *testing.T) {
 	rnd := rand.New(rand.NewSource(123))
 	// test square roots mod 5^i 7^j 11^k for all quadratic residues mod those numbers.
-	var primepowers = [3]primePower{{5, 0}, {7, 0}, {11, 0}}
+	var primepowers = [3]PrimePower{{5, 0}, {7, 0}, {11, 0}}
 	for i := uint(0); i < 4; i++ {
-		primepowers[0].k = i
+		primepowers[0].K = i
 		for j := uint(0); j < 4; j++ {
-			primepowers[1].k = j
+			primepowers[1].K = j
 			for k := uint(0); k < 3; k++ {
-				primepowers[2].k = k
+				primepowers[2].K = k
 
 				n := int64(1)
 				for _, pp := range primepowers {
-					for z := uint(0); z < pp.k; z++ {
-						n *= pp.p
+					for z := uint(0); z < pp.K; z++ {
+						n *= pp.P
 					}
 				}
 				for a := int64(0); a < n; a++ {
-					if gcd(a, n) != 1 {
+					if GCD(a, n) != 1 {
 						continue
 					}
-					if !quadraticResidue(a%5, 5) {
+					if !QuadraticResidue(a%5, 5) {
 						continue
 					}
-					if !quadraticResidue(a%7, 7) {
+					if !QuadraticResidue(a%7, 7) {
 						continue
 					}
-					if !quadraticResidue(a%11, 11) {
+					if !QuadraticResidue(a%11, 11) {
 						continue
 					}
-					x := sqrtModN(a, primepowers[:], rnd)
+					x := SqrtModN(a, primepowers[:], rnd)
 					if x*x%n != a {
 						t.Errorf("bad sqrtModN a=%d n=%d x=%d\n", a, n, x)
 					}
@@ -173,34 +173,34 @@ func TestSqrtModN(t *testing.T) {
 func TestBigSqrtModN(t *testing.T) {
 	rnd := rand.New(rand.NewSource(123))
 	// test square roots mod 5^i 7^j 11^k for all quadratic residues mod those numbers.
-	var primepowers = [3]primePower{{5, 0}, {7, 0}, {11, 0}}
+	var primepowers = [3]PrimePower{{5, 0}, {7, 0}, {11, 0}}
 	for i := uint(0); i < 4; i++ {
-		primepowers[0].k = i
+		primepowers[0].K = i
 		for j := uint(0); j < 4; j++ {
-			primepowers[1].k = j
+			primepowers[1].K = j
 			for k := uint(0); k < 3; k++ {
-				primepowers[2].k = k
+				primepowers[2].K = k
 
 				n := int64(1)
 				for _, pp := range primepowers {
-					for z := uint(0); z < pp.k; z++ {
-						n *= pp.p
+					for z := uint(0); z < pp.K; z++ {
+						n *= pp.P
 					}
 				}
 				for a := int64(0); a < n; a++ {
-					if gcd(a, n) != 1 {
+					if GCD(a, n) != 1 {
 						continue
 					}
-					if !quadraticResidue(a%5, 5) {
+					if !QuadraticResidue(a%5, 5) {
 						continue
 					}
-					if !quadraticResidue(a%7, 7) {
+					if !QuadraticResidue(a%7, 7) {
 						continue
 					}
-					if !quadraticResidue(a%11, 11) {
+					if !QuadraticResidue(a%11, 11) {
 						continue
 					}
-					x := bigSqrtModN(big.Int64(a), primepowers[:], rnd)
+					x := BigSqrtModN(big.Int64(a), primepowers[:], rnd)
 					if x.Square().Mod64(n) != a {
 						t.Errorf("bad bigSqrtModN a=%d n=%d x=%d\n", a, n, x)
 					}
@@ -217,7 +217,7 @@ func TestQuadraticModP(t *testing.T) {
 		for a := int64(1); a < p; a++ {
 			for b := int64(0); b < p; b++ {
 				for c := int64(0); c < p; c++ {
-					s := quadraticModP(a, b, c, p, rnd)
+					s := QuadraticModP(a, b, c, p, rnd)
 					if len(s) > 2 {
 						t.Errorf("too many quadratic solutions")
 					}
--- sieve/sieve_test.go
+++ sieve/sieve_test.go
@@ -14,13 +14,14 @@ func TestSieve(t *testing.T) {
 	b := big.Int64(-9813)
 	c := big.Int64(1011)
 	x0 := big.Int64(426) // ~root of ax^2+bx+c
+	x1 := x0.Add64(1 << 24)
 
 	rnd := rand.New(rand.NewSource(123))
 
 	// Pick a small factor base.  Include primes only if p divides ax^2+bx+c for some x.
 	// Equivalently, only if ax^2+bx+c===0 mod p has a solution.
 	// That happens only if b^2-4ac is a quadratic residue.
-	d := a.Mul(a).Sub(b.Mul(b).Sub(a.Mul(c).Lsh(2)))
+	d := b.Mul(b).Sub(a.Mul(c).Lsh(2))
 	fb := []int64{-1}
 	for i := 0; i < 100; i++ {
 		p := primes.Get(i)
@@ -29,19 +30,22 @@ func TestSieve(t *testing.T) {
 		}
 	}
 
-	results := sievesmooth(a, b, c, fb, x0, rnd)
+	results := Smooth(a, b, c, fb, x0, x1, rnd)
 
 	if len(results) == 0 {
 		t.Errorf("no results for a=%d b=%d c=%d", a, b, c)
 	}
 
 	for _, r := range results {
-		y := a.Mul(r.x).Add(b).Mul(r.x).Add(c)
+		y := a.Mul(r.X).Add(b).Mul(r.X).Add(c)
 		z := big.One
-		for _, f := range r.factors {
+		for _, f := range r.Factors {
 			z = z.Mul64(fb[f])
 		}
-		z = z.Mul64(r.remainder)
+		if r.Remainder != 1 && !big.Int64(r.Remainder).ProbablyPrime(1000) {
+			t.Errorf("bad remainder %d: %v", r.Remainder, r)
+		}
+		z = z.Mul64(r.Remainder)
 		if !z.Equals(y) {
 			t.Errorf("bad sieve result z=%d, want %d", z, y)
 		}
